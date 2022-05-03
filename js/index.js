aef624665a89f564122618b29443a5ad44c67b0d
joseph-willow <josef.salix@gmail.com>
2022-05-03 21:06:26 +0200

    add if statements and boolean variables

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@
 let mysteryNumber = 50;
 let playerGuess = 0;
 
+let guessesRemaining = 10;
+let guessesMade = 0;
+let gameState = "";
+
+let gameWon = false;
+
 //~ the input and output fields
 
 let input = document.querySelector("#input");
@@ -19,20 +25,43 @@ function clickHandler() {
 	}
 	
 function playGame() {
+	
+	guessesRemaining = guessesRemaining - 1;
+	guessesMade = guessesMade + 1;
+	gameState = "Guess: " + guessesMade + " Remaining: " + guessesRemaining;
+	
 	playerGuess = parseInt(input.value);
 	
 	if(playerGuess > mysteryNumber) {
-		output.innerHTML = "that's too high.";
+		output.innerHTML = "that's too high. " + gameState;
+		
+		//~ check for the end of the game
+		if(guessesRemaining < 1) {
+			endGame();
+			}
 		}
 	else if(playerGuess < mysteryNumber) {
-		output.innerHTML = "that's too low";
+		output.innerHTML = "that's too low. " + gameState;
+		
+		//~ check for the end of the game
+		if(guessesRemaining < 1) {
+			endGame();
+			}
 		}
 	else if(playerGuess === mysteryNumber) {
-		output.innerHTML = "you got it!";
+		gameWon = true;
+		endGame();
 		}
 	}
 
-
+function endGame() {
+	if(gameWon) {
+		output.innerHTML = "Yes, it's " + mysteryNumber + "!" + "<br>" + "It only took you " + guessesMade + " guesses.";
+		}
+	else {
+		output.innerHTML = "No more guesses left!" + "<br>" + "The number was: " + mysteryNumber + ".";
+		}
+	}
 
 
 
